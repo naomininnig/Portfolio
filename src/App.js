016545fca1a3d3fa0b501537bc6cbb9bb2977273
naomininnig <naomi.ninnig@gmail.com>
2022-05-11 10:41:35 +0200

    cookies

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Footer from "./componenets/Footer";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   return (
@@ -17,6 +18,28 @@ function App() {
         </Routes>
         <Footer />
       </HashRouter>
+      <CookieConsent
+        style={{
+          background: "#222",
+          opacity: "0.9",
+          color: "#fff",
+          textAlign: "center",
+          fontSize: "18px",
+        }}
+        buttonStyle={{
+          backgroundColor: "rgba(204, 24, 87, 1)",
+          color: "#fff",
+          fontSize: "16px",
+          fontWeight: "500",
+          borderRadius: "8px",
+        }}
+        buttonText="Ok Great!"
+        expires={150}
+      >
+        We use cookies on this site to enhance your user experience. For a
+        complete overview of all cookies used, please see our{" "}
+        <a href="/privacy">Cookie policy.</a>
+      </CookieConsent>
     </>
   );
 }
